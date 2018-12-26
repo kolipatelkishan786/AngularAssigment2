740f712dabfdb9b4f951ffbe5c9a9c26d390484e
Kishan Patel <kolipatelkishan786@gmail.com>
2018-12-26 21:01:40 +0530

    newproject

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.scss']
+})
+export class CockpitComponent implements OnInit {
+  newServerName = [];
+  newServercontent = [];
+  constructor() { }
+
+  ngOnInit() {
+  }
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServercontent
+    });
+  }
+
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: 'Blueprint',
+      name: this.newServerName,
+      content: this.newServercontent
+    });
+  }
+
+}
